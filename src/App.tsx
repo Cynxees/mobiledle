32db06470c8e0555971b46dbcc3b33dd0f94e20e
Derwin <notderwin@gmail.com>
2024-04-16 21:51:06 +1000

    tsconfig

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css'
 import LandingPage from './pages/LandingPage';
@@ -10,9 +7,6 @@ import DevPage from './pages/DevPage';
 
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  
 
   return (
     <BrowserRouter>
