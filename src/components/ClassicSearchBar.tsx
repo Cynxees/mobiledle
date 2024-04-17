7da01f7259aa89b18fc9389b10a0a98b55900ff8
Derwin <notderwin@gmail.com>
2024-04-17 16:02:50 +1000

    design searchbar ( ganti pls jelek )

--- src/components/ClassicSearchBar.tsx
+++ src/components/ClassicSearchBar.tsx
@@ -42,7 +42,6 @@ export default function ClassicSearchBar({ characters }: ClassicSearchBarProps) 
 
     return (
     
-        <form className="max-w-md mx-auto">   
             <div className="relative">
                 <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
                     <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
@@ -52,17 +51,18 @@ export default function ClassicSearchBar({ characters }: ClassicSearchBarProps) 
                 <input 
                     type="text" 
                     id="search-bar" 
-                    autoComplete="false"
+                    autoComplete="off"
                     className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
                     placeholder="Heroes" 
                     value = {prefix}
                     onChange = {onInput}
                 />
 
+                <div className="absolute w-full">
                 {suggestions.map((item : String) => (
-                    <li key="item">{item}</li>
+                    <li key="item" className="bg-gray-600 w-full list-none py-2 border-gray-400 border">{item}</li>
                 ))}
+                </div>
             </div>
-        </form>
   );
 }
