b6edf9f89fa7cc6d0683972db2c0d2a50d1fc3c7
Chris <christopherhuu77@gmail.com>
2024-04-18 22:27:09 +0700

    haha

--- src/components/classic/ClassicTableTitle.tsx
+++ src/components/classic/ClassicTableTitle.tsx
@@ -1,6 +1,6 @@
 export default function ClassicTableTitle() {
   return (
-    <section className="flex gap-2 w-full flex-row justify-evenly">
+    <section className="flex w-full gap-2 justify-center">
       <div className="border-b-2 pb-2 border-[#B88851] w-20">Hero</div>
       <div className="border-b-2 pb-2 border-[#B88851] w-20">Gender</div>
       <div className="border-b-2 pb-2 border-[#B88851] w-20">Role</div>
