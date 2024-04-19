df56e9b20742265fee325e8a81dd5a51e6b12db5
Derwin <notderwin@gmail.com>
2024-04-20 02:07:24 +1000

    survival

--- src/pages/SurvivalPage.tsx
+++ src/pages/SurvivalPage.tsx
@@ -9,32 +9,84 @@ import Navbar from "../components/Navbar";
 
 function getRandomStat(character: MobileLegendsCharacter) {
 
-    let random = Math.floor(Math.random()*100)%16
+    let random = Math.floor(Math.random()*100)%12
 
     // if( random => 7 && random <= 9) return getRandomStat(character)
 
-    switch(random){
-        case 0 : return character.name
-        case 1 : return character.alias
-        case 2 : return character.gender
-        case 3 : return character.role
-        case 4 : return character.specialty
-        case 5 : return character.lane
-        case 6 : return character.region
-        case 7 : return character.goldPrice
-        case 8 : return character.ticketPrice
-        case 9 : return character.diamondPrice
-        case 10: return character.year
-        case 11 : return character.rangeType
-        case 12 : return character.damageType
-        case 13 : return character.resource
-        case 14 : return character.hairColor
-        case 15 : return character.species
-    }
-    
+    let characterStats = [
+        character.alias, 
+        character.gender, 
+        character.role, 
+        character.specialty,
+        character.lane,
+        character.region,
+        character.year,
+        character.rangeType,
+        character.damageType,
+        character.resource,
+        character.hairColor,
+        character.species
+    ]
+
+    return characterStats[random]
 
 
 }
+
+function getWrongStat(character: MobileLegendsCharacter, characters: MobileLegendsCharacter[]){
+    
+    let characterStats = [
+        character.alias, 
+        character.gender, 
+        character.role, 
+        character.specialty,
+        character.lane,
+        character.region,
+        character.year,
+        character.rangeType,
+        character.damageType,
+        character.resource,
+        character.hairColor,
+        character.species
+    ]
+
+    while(true){
+        let random = Math.floor(Math.random()*100)%12
+        let randomCharacter = characters[Math.floor(Math.random()*100)%characters.length]
+
+        let randomCharacterStats = [
+            randomCharacter.alias, 
+            randomCharacter.gender, 
+            randomCharacter.role, 
+            randomCharacter.specialty,
+            randomCharacter.lane,
+            randomCharacter.region,
+            randomCharacter.year,
+            randomCharacter.rangeType,
+            randomCharacter.damageType,
+            randomCharacter.resource,
+            randomCharacter.hairColor,
+            randomCharacter.species
+        ]
+
+        if(randomCharacterStats[random] != characterStats[random]) return randomCharacterStats[random]
+
+        
+    }
+
+}
+
+function getRandomCharacter(characters : MobileLegendsCharacter[]){
+
+        
+    let random = Math.floor(Math.random()*100)%30
+
+
+    return characters[random]
+
+
+}
+
 
 export default function SurvivalPage() {
     const { characters, isLoading } = useMobileLegendsCharacters();
@@ -55,8 +107,32 @@ export default function SurvivalPage() {
 
     const [leftText, setLeftText]= useState("")
     const [rightText, setRightText]= useState("")
+    const [rightIsCorrect, setRightCorrect] = useState(false)
     const [isAllLoaded, setIsAllLoaded] = useState(false);
     const [gameStarted, setGameStarted] = useState(false);
+
+    function startRound(){
+
+        setCountdown(10)
+
+        let correctPrompt = getRandomStat(currentCharacter).toString()
+        let wrongPrompt = getWrongStat(currentCharacter, characters).toString()
+        
+        if(countdown%2 == 0){
+            setLeftText(correctPrompt)
+            setRightText(wrongPrompt)
+            setRightCorrect(false)
+        }else{
+            
+            setRightText(correctPrompt)
+            setLeftText(wrongPrompt)
+            setRightCorrect(true)
+        }
+
+
+
+    }
+
 
 
     useEffect(() => {
@@ -94,7 +170,7 @@ export default function SurvivalPage() {
             const imagesPromiseList: Promise<any>[] = []
             characters.forEach(c =>{
 
-                imagesPromiseList.push(preloadImage(c.imageUrl[0]))
+                if(c.imageUrl[1] != null && c.imageUrl[1] != undefined && c.id < 30) imagesPromiseList.push(preloadImage(c.imageUrl[1]))
             }
 
             )
@@ -114,7 +190,7 @@ export default function SurvivalPage() {
         if(!isAllLoaded) return
         const fillCurrentCharacter = () => {
             console.log("starting round")
-            setRandomCurrentCharacter()
+            setCurrentCharacter(getRandomCharacter(characters))
 
 
         }
@@ -127,24 +203,14 @@ export default function SurvivalPage() {
 
         if(!currentCharacter) return
 
-        setLeftText(getRandomStat(currentCharacter).toString())
-        setRightText(getRandomStat(currentCharacter).toString())
+        startRound()
 
         setCountdownActive(true)
         setGameStarted(true)
 
     }, [currentCharacter])
 
-    function setRandomCurrentCharacter(){
-
-        
-        let random = Math.floor(Math.random()*100)%characters.length
-
-        setCurrentCharacter(characters[0])
-
-
-    }
-
+    
 
     const handleDrag = (e : DraggableEvent, data : DraggableData) => {
 
@@ -172,18 +238,21 @@ export default function SurvivalPage() {
 
 
 
-        setCharactersRotation(data.lastX*0.5**3)
+        setCharactersRotation(data.lastX*0.4**5)
         setX(newX)
 
         setCharactersOpacity(Math.abs(x)/150)
 
-        if(x>0){
+        if(x>70){
             setRightOpacity(Math.abs(x)/500)
             setLeftOpacity(0)
-        }else{
+        }else if(x<-70){
             
             setRightOpacity(0)
             setLeftOpacity(Math.abs(x)/400)
+        }else{
+            setRightOpacity(0)
+            setLeftOpacity(0)
         }
         
         
@@ -192,8 +261,47 @@ export default function SurvivalPage() {
     }
 
     const handleStop = (e: DraggableEvent, data: DraggableData) => {
-        console.log('Drag stopped', data);
+        let stoppedX = data.x
+
+        if(stoppedX < -70){
+
+            if(!rightIsCorrect) handleCorrect() 
+                else handleWrong()
+
+        }else if(stoppedX > 70){
+            if(rightIsCorrect) handleCorrect() 
+                else handleWrong()
+        
+        }else{
+
+
+        }
+        
+        setRightOpacity(0)
+        setLeftOpacity(0)
+        setX(0)
+
     };
+
+    function handleCorrect(){
+
+
+        
+        setCurrentCharacter(getRandomCharacter(characters))
+        startRound()
+        setScore(score+1)
+        
+
+    }
+
+
+    function handleWrong(){
+
+        console.log("LOSS")
+        setCountdown(0)
+        setScore(0)
+
+    }
 
 
     
@@ -211,6 +319,8 @@ export default function SurvivalPage() {
             <video src="/survivalVideo.mp4" className="min-w-full min-h-full blur-xl opacity-10" autoPlay muted loop></video>
 
         </div>
+
+        <div className="bg-gray-900 rounded-lg  border-white border-2 p-20 ">
         <Navbar>
 
         </Navbar>
@@ -219,15 +329,14 @@ export default function SurvivalPage() {
 
 
 
-            <div className="h-full relative flex -z-10 ">
-            <div className={`${(leftOpacity>0) ? 'animate-pulse' :  '' } bg-orange-300 blur-3xl w-full h-full absolute`} style={{ opacity: 0.1 +leftOpacity }}/>
-            <div className="w-full my-auto items-center align-middle text-3xl relative z-00 text-shadow-lg shadow-black motion-reduce:animate-bounce" style={{ opacity: 0.2+leftOpacity*5 }}>
+            <div className="h-full w-60 relative flex z-30">
+            <div className={`${(leftOpacity>0) ? 'animate-pulse' :  'animate-none' } bg-orange-300 blur-3xl w-full h-full absolute`} style={{ opacity: 0.1 +leftOpacity }}/>
+            <div className="w-full my-auto text-orange-200 brightness-110 items-center align-middle text-3xl relative z-00 text-shadow-lg shadow-black motion-reduce:animate-bounce" style={{ opacity: 0.2+leftOpacity*5 }}>
             {leftText}
             </div>
             </div>
             
-                
-
+            
             <div className="z-10 relative">
 
             <div className={`font-nova-bold text-6xl mb-10 ${countdownClass}`}>
@@ -244,28 +353,30 @@ export default function SurvivalPage() {
                 defaultPosition={{ x: 0, y: 0 }}
                 grid={[10, 0]}
                 scale={1}
+                
             >
-                <div className="handle relative z-10 mb-20 transform " style={{ WebkitFilter: `brightness(${characterOpacity + 0.6})`, filter : `brightness(100%)` }}>
+                <div className="handle relative z-30 mb-20 transform " style={{ WebkitFilter: `brightness(${characterOpacity + 0.6})`, filter : `brightness(100%)` }}>
 
                 <div className="bg-image absolute inset-0 scale-[2] bg-center  w-full  blur-3xl opacity-80 motion-reduce:animate-bounce animate-pulse" 
                     style={{ backgroundImage: `url(${currentCharacter?.imageUrl[1]})`, backgroundSize: 'cover', transform: `rotate(${characterRotation}deg)` }} />
                     
                 <div style={{ transform: `rotate(${characterRotation}deg)` }}>{currentCharacter?.name}
-                <img className="rounded-xl w-full max-w-[50vw] relative z-10" src={currentCharacter?.imageUrl[1]} alt={currentCharacter?.name} draggable={false}></img></div>
-
+                <img className="rounded-xl w-60 h-96  relative z-10" src={currentCharacter?.imageUrl[1]} alt={currentCharacter?.name} draggable={false}></img></div>
 
 
                 </div>
             </Draggable>
+            <div className="w-full px-10 py-2 border border-yellow-100 border-2 rounded-xl">{score}</div>
             </div>
 
-            <div className="h-full relative flex -z-10 ">
+            <div className="h-full w-60 relative flex z-10 ">
             <div className={`${(rightOpacity>0) ? 'animate-pulse' :  '' } bg-orange-300 blur-3xl w-full h-full absolute`} style={{ opacity: 0.1+rightOpacity }}/>
-            <div className="w-full my-auto items-center align-middle text-3xl relative z-00 text-shadow-lg shadow-black motion-reduce:animate-bounce" style={{ opacity: 0.3+rightOpacity*5 }}>
+            <div className="w-full my-auto text-wrap text-orange-200 brightness-110  items-center align-middle text-3xl relative z-10 text-shadow-lg shadow-black motion-reduce:animate-bounce" style={{ opacity: 0.3+rightOpacity*5 }}>
             {rightText}
             </div>
             </div>
 
+        </div>
         </div>
         </div>
     );
