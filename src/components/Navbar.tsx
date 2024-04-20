70bd7394057ed8709df3c80b334773643b9317bd
Chris <christopherhuu77@gmail.com>
2024-04-20 13:43:32 +0700

    a

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,17 +2,29 @@ import { IoMdSettings } from "react-icons/io";
 import { FaLanguage } from "react-icons/fa6";
 import "animate.css";
 import { useTranslation } from "react-i18next";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 export default function Navbar() {
   const { t, i18n } = useTranslation();
 
-  const [currentLanguage, setCurrentLanguage] = useState<string>("en");
+  const [currentLanguage, setCurrentLanguage] = useState<string>(() => {
+    const savedLanguage = localStorage.getItem("savedLanguage");
+    i18n.changeLanguage(savedLanguage);
+    return savedLanguage;
+  });
 
   const changeLanguage = (lng: any) => {
-    currentLanguage === "en" ? setCurrentLanguage("id") : setCurrentLanguage("en")
+    currentLanguage === "en"
+      ? setCurrentLanguage("id")
+      : setCurrentLanguage("en");
 
-    i18n.changeLanguage(lng); // Function to change the language
+    localStorage.setItem("savedLanguage", lng);
+
+    i18n.changeLanguage(lng);
   };
+
+  useEffect(() => {
+    localStorage.setItem("savedLanguage", "en");
+  }, []);
 
   return (
     <nav className="flex justify-between items-center">
@@ -21,13 +33,9 @@ export default function Navbar() {
         MOBILEDLE
       </div>
       {currentLanguage === "en" ? (
-        <button  onClick={() => changeLanguage("id")}>
-          English
-        </button>
+        <button onClick={() => changeLanguage("id")}>English</button>
       ) : (
-        <button  onClick={() => changeLanguage("en")}>
-          Indonesia
-        </button>
+        <button onClick={() => changeLanguage("en")}>Indonesia</button>
       )}
       {/* <span>
         <button className="w-full mb-3" onClick={() => changeLanguage("en")}>English</button>
