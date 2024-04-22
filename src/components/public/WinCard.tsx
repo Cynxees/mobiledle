c5bb64652cc29d5d2df38510319efdbae683dbc8
Chris <christopherhuu77@gmail.com>
2024-04-22 08:14:13 +0700

    update translation

--- src/components/public/WinCard.tsx
+++ src/components/public/WinCard.tsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useTranslation } from "react-i18next";
 import SurvivalBox from "../../components/navigation/SurvivalBox";
 
 interface WinCardProps {
@@ -6,13 +6,14 @@ interface WinCardProps {
 }
 
 const WinCard: React.FC<WinCardProps> = ({ winCardRef }) => {
+  const {t} = useTranslation();
   return (
     <div
       className="flex flex-col items-start gap-3 border-2 border-[#fff] bg-[#d88d43] p-5 w-1/2 rounded-lg"
       ref={winCardRef} // Attach the ref to the div
     >
-      <h2 className="text-white border-b-2 border-b-white w-full text-left">Well Done!</h2>
-      <h2 className="text-white">Next Mode : </h2>
+      <h2 className="text-white border-b-2 border-b-white w-full text-left">{t`Well Done!`}</h2>
+      <h2 className="text-white">{t`Next Mode`} : </h2>
       <SurvivalBox />
     </div>
   );
