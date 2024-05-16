8c2eb2f68310f9fe67290260bbf9847a2bd162d4
Derwin <notderwin@gmail.com>
2024-05-17 01:58:30 +1000

    remove desktop.ini, expand hero 21 => 40

--- src/pages/DevPage5.tsx
+++ src/pages/DevPage5.tsx
@@ -24,6 +24,7 @@ export default function DevPage() {
             prefix: searchPrefix,
             options: {
                 accessLevel: "guest",
+                listAll: true
 
             }
             
